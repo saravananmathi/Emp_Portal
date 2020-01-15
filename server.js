d4d1f298e7da453a28a08e371e0c66ec91491950
unknown <skanagaraj@dminc.com>
2020-01-15 16:47:37 +0530

    Added Employee form and pg

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const scss = require('node-sass');
+const bodyPaser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
@@ -11,10 +12,19 @@ app.engine('mustache',mustache);
 app.set('view engine' , 'mustache');
 
 app.use(express.static('root'));
+app.use(bodyPaser.urlencoded({ extended:false }));  
 
 app.get('/list',(req,res)=>{
     res.render('list')
 });
+
+app.get('/Employee/add',(req,res)=>{
+    res.render('EmployeeForm')
+})
+ app.post('/Employee/add',(req,res)=>{
+     res.redirect('/list');
+     console.log(req.body)
+ })
 
 app.listen(process.env.port,(req,res)=>{
     console.log(`Listing at port ${process.env.port}`);
