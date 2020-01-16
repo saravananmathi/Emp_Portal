42a3c7e5ef2d35050a5fa58ad4ada4f5198417bb
fathima.nassrin@gmail.com <fathima.nassrin@gmail.com>
2020-01-16 16:23:41 +0530

    Commit Successful

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const scss = require('node-sass');
 const bodyPaser = require('body-parser');
+const {Client} = require('pg');
+
 require('dotenv').config();
 
 const app = express();
@@ -15,13 +17,36 @@ app.use(express.static('root'));
 app.use(bodyPaser.urlencoded({ extended:false }));  
 
 app.get('/list',(req,res)=>{
-    res.render('list')
+    var result ;
+    const client = new Client();  
+ client.connect().then(()=>{
+  return  client.query('Select * from emp',(err,res)=>{
+
+        result = res.rows;
+    });
+ 
+ });
+    res.render('list',result);
+    console.log(result);
 });
 
 app.get('/Employee/add',(req,res)=>{
     res.render('EmployeeForm')
 })
  app.post('/Employee/add',(req,res)=>{
+    const client = new Client();
+    client.connect().then(()=>{
+    var sql = 'insert into emp (empno,ename,job) values ($1,$2,$3);'
+    var params = [];
+    params = [104,req.body.EmployeeName,req.body.Designation];
+    client.query(sql,params,(err,res)=>{
+        console.log(err,res);
+    });
+    }).catch(err =>{
+        console.log(err);;
+
+});
+
      res.redirect('/list');
      console.log(req.body)
  })
